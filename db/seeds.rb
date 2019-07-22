801357efc4c1e75b39b90f6075e50ed8880ff8b8
yusai13 <future.gazer7@gmail.com>
2019-07-22 22:49:40 +0900

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create(name: "雷神のWebマーケティング", price: 1500, user_id: 1)
+Item.create(name: "水神シンキング", price: 1200, user_id: 1)
+Item.create(name: "風神以外、かすり傷。", price: 1300, user_id: 1)
+
+#seedsは初期データを入れるところ
